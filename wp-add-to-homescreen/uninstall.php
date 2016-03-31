88eef77c1634508c9a352cb100bee4c6d4cbc64d
Salvador de la Puente González <salva@unoyunodiez.com>
2016-03-31 13:53:37 +0200

    Fixing legacy code made unistalling the plugin impossible

--- wp-add-to-homescreen/uninstall.php
+++ wp-add-to-homescreen/uninstall.php
@@ -3,7 +3,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
-include_once(plugin_dir_path(__FILE__) . 'class-wp-offline-content-options.php');
+include_once(plugin_dir_path(__FILE__) . 'class-wp-add-to-homescreen-options.php');
 
 WP_Offline_Content_Options::get_options()->remove_all();
 ?>
